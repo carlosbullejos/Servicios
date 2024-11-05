1e417266d4228f377a61a2d04962245ad8716ec9
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2024-11-06 00:19:34 +0100

    Update terraform.tf

--- vpcs/terraform.tf
+++ vpcs/terraform.tf
@@ -115,6 +115,13 @@ resource "aws_security_group" "ftp_security_group" {
     cidr_blocks = [var.allowed_cidr]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_cidr]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
