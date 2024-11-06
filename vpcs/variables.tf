065e546af4e75168e58eaa1ab5c8bcc0c506b147
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2024-11-06 19:54:13 +0100

    Update variables.tf

--- vpcs/variables.tf
+++ vpcs/variables.tf
@@ -1,76 +1,79 @@
 variable "aws_region" {
-  description = "Región para AWS"
+  description = "AWS Region"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "vpc1_cidr_block" {
-  description = "Red para la vpc1"
+  description = "CIDR block for VPC1"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "vpc1_public_subnet_cidr" {
-  description = "subred pública de la vpc1"
+  description = "CIDR block for VPC1 public subnet"
   type        = string
-  default     = "10.0.1.0/24"
 }
 
 variable "vpc1_private_subnet_cidr" {
-  description = "subred privada de la vpc1"
+  description = "CIDR block for VPC1 private subnet"
   type        = string
-  default     = "10.0.2.0/24"
 }
 
 variable "vpc2_cidr_block" {
-  description = "Red para la vpc2"
+  description = "CIDR block for VPC2"
   type        = string
-  default     = "10.1.0.0/16"
 }
 
 variable "vpc2_private_subnet_cidr" {
-  description = "subred privada para la vpc2"
+  description = "CIDR block for VPC2 private subnet"
   type        = string
-  default     = "10.1.1.0/24"
 }
 
 variable "availability_zone1" {
-  description = "Zona 1"
+  description = "Availability zone for VPC1"
   type        = string
-  default     = "us-east-1a"
 }
 
 variable "availability_zone2" {
-  description = "Zona 2 en caso de que la zona 1 no  esté disponible"
+  description = "Availability zone for VPC2"
   type        = string
-  default     = "us-east-1b"
 }
 
 variable "allowed_cidr" {
-  description = "Red para permitir todo el tráfico"
+  description = "CIDR block for allowed IPs"
   type        = string
-  default     = "0.0.0.0/0"
 }
 
 variable "instance_ami" {
-  description = "Instancia debian ."
+  description = "AMI ID for the instance"
   type        = string
-  default     = "ami-064519b8c76274859"
 }
 
 variable "instance_type" {
-  description = "Tipo de instancia."
+  description = "Type of instance"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Nombre de la clave para ssh"
+  description = "The name of the SSH key pair"
   type        = string
 }
 
 variable "user_data" {
-  description = "Script que se ejecuta en la instancia en el momento de creación."
+  description = "User data script"
   type        = string
-  default     = "script.sh"
+}
+
+variable "bastion_ip" {
+  description = "IP address of the bastion host"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket for FTP"
+  type        = string
+}
+
+variable "ftp_container_name" {
+  description = "Name of the Docker container for FTP"
+  type        = string
 }
