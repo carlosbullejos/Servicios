b903a4710ad28a43abf8ba2cfb6e56236de42cfc
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2024-11-06 09:00:59 +0100

    Update variables.tf

--- vpcs/variables.tf
+++ vpcs/variables.tf
@@ -1,76 +1,76 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources."
+  description = "Región para AWS"
   type        = string
   default     = "us-east-1"
 }
 
 variable "vpc1_cidr_block" {
-  description = "CIDR block for VPC1."
+  description = "Red para la vpc1"
   type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "vpc1_public_subnet_cidr" {
-  description = "CIDR block for public subnet in VPC1."
+  description = "subred pública de la vpc1"
   type        = string
   default     = "10.0.1.0/24"
 }
 
 variable "vpc1_private_subnet_cidr" {
-  description = "CIDR block for private subnet in VPC1."
+  description = "subred privada de la vpc1"
   type        = string
   default     = "10.0.2.0/24"
 }
 
 variable "vpc2_cidr_block" {
-  description = "CIDR block for VPC2."
+  description = "Red para la vpc2"
   type        = string
   default     = "10.1.0.0/16"
 }
 
 variable "vpc2_private_subnet_cidr" {
-  description = "CIDR block for private subnet in VPC2."
+  description = "subred privada para la vpc2"
   type        = string
   default     = "10.1.1.0/24"
 }
 
 variable "availability_zone1" {
-  description = "Availability zone for VPC1."
+  description = "Zona 1"
   type        = string
   default     = "us-east-1a"
 }
 
 variable "availability_zone2" {
-  description = "Availability zone for VPC2."
+  description = "Zona 2 en caso de que la zona 1 no  esté disponible"
   type        = string
   default     = "us-east-1b"
 }
 
 variable "allowed_cidr" {
-  description = "CIDR block allowed for FTP ports."
+  description = "Red para permitir todo el tráfico"
   type        = string
   default     = "0.0.0.0/0"
 }
 
 variable "instance_ami" {
-  description = "AMI ID for the FTP instance."
+  description = "Instancia debian ."
   type        = string
   default     = "ami-064519b8c76274859"
 }
 
 variable "instance_type" {
-  description = "Instance type for the FTP instance."
+  description = "Tipo de instancia."
   type        = string
   default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Name of the SSH key to use for the instance."
+  description = "Nombre de la clave para ssh"
   type        = string
 }
 
 variable "user_data" {
-  description = "Path to the user data script file."
+  description = "Script que se ejecuta en la instancia en el momento de creación."
   type        = string
   default     = "script.sh"
 }
