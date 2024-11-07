bdc569abdc8a19ea649847426318838c8381ac59
carlosbullejos <117440194+carlosbullejos@users.noreply.github.com>
2024-11-07 13:40:22 +0100

    Update terraform.tf

--- vpcs/terraform.tf
+++ vpcs/terraform.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
+# VPC 1 con subred pública y privada
 resource "aws_vpc" "vpc1" {
   cidr_block = var.vpc1_cidr_block
 }
@@ -23,6 +24,7 @@ resource "aws_subnet" "vpc1_private_subnet" {
   availability_zone = var.availability_zone1
 }
 
+# VPC 2 con subred privada
 resource "aws_vpc" "vpc2" {
   cidr_block = var.vpc2_cidr_block
 }
@@ -33,12 +35,14 @@ resource "aws_subnet" "vpc2_private_subnet" {
   availability_zone = var.availability_zone2
 }
 
+# Peering entre VPCs
 resource "aws_vpc_peering_connection" "vpc1_vpc2_peering" {
   vpc_id      = aws_vpc.vpc1.id
   peer_vpc_id = aws_vpc.vpc2.id
   auto_accept = true
 }
 
+# Tabla de rutas para el tráfico de internet en la subred pública de VPC 1
 resource "aws_route_table" "vpc1_public_route_table" {
   vpc_id = aws_vpc.vpc1.id
 
@@ -53,6 +57,7 @@ resource "aws_route_table_association" "vpc1_public_route_assoc" {
   route_table_id = aws_route_table.vpc1_public_route_table.id
 }
 
+# Tabla de rutas privada en VPC 1
 resource "aws_route_table" "vpc1_private_route_table" {
   vpc_id = aws_vpc.vpc1.id
 }
@@ -62,6 +67,7 @@ resource "aws_route_table_association" "vpc1_private_route_assoc" {
   route_table_id = aws_route_table.vpc1_private_route_table.id
 }
 
+# Tabla de rutas privada en VPC 2
 resource "aws_route_table" "vpc2_private_route_table" {
   vpc_id = aws_vpc.vpc2.id
 }
@@ -71,6 +77,7 @@ resource "aws_route_table_association" "vpc2_private_route_assoc" {
   route_table_id = aws_route_table.vpc2_private_route_table.id
 }
 
+# Rutas de peering entre VPCs
 resource "aws_route" "route_vpc1_to_vpc2" {
   route_table_id           = aws_route_table.vpc1_private_route_table.id
   destination_cidr_block   = aws_vpc.vpc2.cidr_block
@@ -83,19 +90,13 @@ resource "aws_route" "route_vpc2_to_vpc1" {
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc1_vpc2_peering.id
 }
 
+# Security Group para el FTP
 resource "aws_security_group" "ftp_security_group" {
   vpc_id = aws_vpc.vpc1.id
   name   = "ftp_security_group"
 
   ingress {
     from_port   = 20
-    to_port     = 20
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_cidr]
-  }
-
-  ingress {
-    from_port   = 21
     to_port     = 21
     protocol    = "tcp"
     cidr_blocks = [var.allowed_cidr]
@@ -103,13 +104,6 @@ resource "aws_security_group" "ftp_security_group" {
 
   ingress {
     from_port   = 1100
-    to_port     = 1100
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_cidr]
-  }
-
-  ingress {
-    from_port   = 1101
     to_port     = 1101
     protocol    = "tcp"
     cidr_blocks = [var.allowed_cidr]
@@ -130,6 +124,7 @@ resource "aws_security_group" "ftp_security_group" {
   }
 }
 
+# Security Group para el Bastion Host
 resource "aws_security_group" "bastion_sg" {
   name        = "bastion_security_group"
   description = "Allow SSH access from a specific IP for Bastion Host"
@@ -139,7 +134,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.bastion_ip]  # IP específica desde donde acceder al bastión
+    cidr_blocks = [var.bastion_ip]  # IP específica para acceder al bastión
   }
 
   egress {
@@ -150,33 +145,40 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-resource "aws_instance" "instancia-ftp" {
+# Instancia FTP
+resource "aws_instance" "instancia_ftp" {
   ami                    = var.instance_ami
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.vpc1_public_subnet.id
   vpc_security_group_ids = [aws_security_group.ftp_security_group.id]
   key_name               = var.key_name
-  user_data              = file(var.user_data)
-  iam_instance_profile   = aws_iam_role.s3_access_role.name
+  user_data              = file("userdata.sh")
   tags = {
     Name = "InstanciaFTP"
   }
 }
 
+# Bastion Host
 resource "aws_instance" "bastion_host" {
-  ami                    = var.instance_ami
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.vpc1_public_subnet.id
-  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  key_name               = var.key_name
-  associate_public_ip_address = true
+  ami                          = var.instance_ami
+  instance_type                = var.instance_type
+  subnet_id                    = aws_subnet.vpc1_public_subnet.id
+  vpc_security_group_ids       = [aws_security_group.bastion_sg.id]
+  key_name                     = var.key_name
+  associate_public_ip_address  = true
   tags = {
     Name = "BastionHost"
   }
 }
 
+# Bucket S3 para almacenamiento FTP
 resource "aws_s3_bucket" "ftp_storage" {
   bucket = "my-ftp-storage-bucket"
+}
+
+# Configuración ACL para el bucket S3 (opcional)
+resource "aws_s3_bucket_acl" "ftp_storage_acl" {
+  bucket = aws_s3_bucket.ftp_storage.id
   acl    = "private"
 }
 
